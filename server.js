de480114cd68a29079953c70032f0943d12a1241
Chetan kumar <chetankumar71297@gmail.com>
2023-08-16 15:53:18 +0530

    Connect to mongodb database

---
+++ server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+
+//configure env
+dotenv.config();
+
+//database config
+connectDB();
+
+//rest object
+const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(morgan("dev"));
+
+//rest api
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to ecommerce web app</h1>");
+});
+
+//PORT
+const PORT = process.env.PORT || 8080;
+
+//run listen
+app.listen(PORT, () => {
+  console.log(`Server Running on ${PORT}`);
+});
