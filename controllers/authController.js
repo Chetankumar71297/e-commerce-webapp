6fd5cd4ebc9e1b91d9050c8d4b6724c44647a01b
Chetan kumar <chetankumar71297@gmail.com>
2023-08-17 21:21:28 +0530

    Create protected middlewares

--- controllers/authController.js
+++ controllers/authController.js
@@ -106,3 +106,13 @@ export const loginController = async (req, res) => {
     });
   }
 };
+
+//test controller
+export const testController = (req, res) => {
+  try {
+    res.send("Protected routes");
+  } catch (error) {
+    console.log(error);
+    res.send({ error });
+  }
+};
