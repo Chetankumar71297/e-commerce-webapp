d4ee4989a96924695fd1a542679e4527e0c2c67f
Chetan kumar <chetankumar71297@gmail.com>
2023-08-26 21:11:07 +0530

    Create admin dashboard and protect this route for admin access only

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,8 @@ import "react-toastify/dist/ReactToastify.css";
 import Dashboard from "./pages/User/Dashboard";
 import PrivateRoute from "./components/Routes/Private";
 import ForgotPassword from "./pages/Auth/ForgotPassword";
+import AdminRoute from "./components/Routes/AdminRoute";
+import AdminDashboard from "./pages/Admin/AdminDashboard";
 function App() {
   return (
     <>
@@ -23,7 +25,10 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/dashboard" element={<PrivateRoute />}>
-          <Route path="" element={<Dashboard />} />
+          <Route path="user" element={<Dashboard />} />
+        </Route>
+        <Route path="/dashboard" element={<AdminRoute />}>
+          <Route path="admin" element={<AdminDashboard />} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
---
+++ client/src/pages/Admin/AdminDashboard.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Layout from "../../components/Layout/Layout";
+const AdminDashboard = () => {
+  return (
+    <Layout title="Admin Dashboard - Ecommerce App">AdminDashboard</Layout>
+  );
+};
+
+export default AdminDashboard;
