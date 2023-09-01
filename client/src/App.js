2ab917c884efda608e27e2935081176dc90092fa
Chetan kumar <chetankumar71297@gmail.com>
2023-09-01 20:24:33 +0530

    Create user dashboard and related pages

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,8 @@ import AdminDashboard from "./pages/Admin/AdminDashboard";
 import CreateCategory from "./pages/Admin/CreateCategory";
 import CreateProduct from "./pages/Admin/CreateProduct";
 import Users from "./pages/Admin/Users";
+import Profile from "./pages/User/Profile";
+import Orders from "./pages/User/Orders";
 function App() {
   return (
     <>
@@ -29,6 +31,8 @@ function App() {
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/dashboard" element={<PrivateRoute />}>
           <Route path="user" element={<Dashboard />} />
+          <Route path="user/profile" element={<Profile />} />
+          <Route path="user/orders" element={<Orders />} />
         </Route>
         <Route path="/dashboard" element={<AdminRoute />}>
           <Route path="admin" element={<AdminDashboard />} />
