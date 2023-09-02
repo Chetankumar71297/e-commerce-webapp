463f885c38ac70b9a07bdd43e972087473ffb6e7
Chetan kumar <chetankumar71297@gmail.com>
2023-09-02 12:39:54 +0530

    Create category api for admin dashboard create-category section

---
+++ controllers/categoryController.js
@@ -0,0 +1,33 @@
+import slugify from "slugify";
+import categoryModal from "../models/categoryModal.js";
+
+export const createCategoryController = async (req, res) => {
+  try {
+    const { name } = req.body;
+    if (!name) {
+      return res.status(401).send({ message: "Name is required" });
+    }
+    const existingCategory = await categoryModal.findOne({ name });
+    if (existingCategory) {
+      return res
+        .status(200)
+        .send({ success: true, message: "Category already exists" });
+    }
+    const category = await new categoryModal({
+      name,
+      slug: slugify(name),
+    }).save();
+    res.status(201).send({
+      success: true,
+      message: "New category created",
+      category,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Error in category",
+    });
+  }
+};
