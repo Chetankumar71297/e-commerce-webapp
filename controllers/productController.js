eb0024aa5f250a7ad3ee92881dc805c9f7530cea
Chetan kumar <chetankumar71297@gmail.com>
2023-09-05 12:45:21 +0530

    Create all the remaining api's for product section

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,7 @@
 import slugify from "slugify";
 import productModel from "../models/productModel.js";
 import fs from "fs";
+
 export const createProductController = async (req, res) => {
   try {
     const { name, description, price, category, quantity, shipping } =
@@ -23,11 +24,9 @@ export const createProductController = async (req, res) => {
           .status(400)
           .send({ error: "Photo is required and should be less then 1mb" });
       case !allowedImageTypes.includes(photo.type):
-        return res
-          .status(400)
-          .send({
-            error: "Invalid file format. Only JPEG, PNG, and GIF are allowed.",
-          });
+        return res.status(400).send({
+          error: "Invalid file format. Only JPEG, PNG, and GIF are allowed.",
+        });
     }
     const product = new productModel({
       ...req.fields,
@@ -49,6 +48,148 @@ export const createProductController = async (req, res) => {
       success: false,
       error,
       message: "Error in creating product",
+    });
+  }
+};
+
+export const getProductsController = async (req, res) => {
+  try {
+    const products = await productModel
+      .find({})
+      .populate("category")
+      .select("-photo")
+      .limit(12)
+      .sort({ createdAt: -1 });
+    res.status(200).send({
+      success: true,
+      totalCount: products.length,
+      message: "All products",
+      products,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Error in getting products",
+    });
+  }
+};
+
+export const getProductController = async (req, res) => {
+  try {
+    const product = await productModel
+      .findOne({ slug: req.params.slug })
+      .populate("category")
+      .select("-photo");
+    res.status(200).send({
+      success: true,
+      message: "Successfully fetched the product",
+      product,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Error in getting product",
+    });
+  }
+};
+
+export const getProductPhotoController = async (req, res) => {
+  try {
+    const product = await productModel.findById(req.params.pid).select("photo");
+    if (product.photo.data) {
+      res.set("Content-type", product.photo.contentType);
+      return res.status(200).send(product.photo.data);
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Error while getting product image",
+    });
+  }
+};
+
+export const deleteProductController = async (req, res) => {
+  try {
+    const deletedProduct = await productModel.findByIdAndDelete(req.params.pid);
+
+    if (!deletedProduct) {
+      // If the product was not found, return a 404 status code
+      return res.status(404).json({
+        success: false,
+        message: "Product not found",
+      });
+    }
+
+    res.status(200).send({
+      success: true,
+      message: "Product deleted successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Error while deleting product",
+    });
+  }
+};
+
+export const updateProductController = async (req, res) => {
+  try {
+    const { name, description, price, category, quantity, shipping } =
+      req.fields;
+    const { photo } = req.files;
+    const allowedImageTypes = ["image/jpeg", "image/png", "image/gif"];
+    switch (true) {
+      case !name:
+        return res.status(400).send({ error: "Name is required" });
+      case !description:
+        return res.status(400).send({ error: "Description is required" });
+      case !price:
+        return res.status(400).send({ error: "Price is required" });
+      case !category:
+        return res.status(400).send({ error: "Category is required" });
+      case !quantity:
+        return res.status(400).send({ error: "Quantity is required" });
+      case !photo || photo.size > 1000000:
+        return res
+          .status(400)
+          .send({ error: "Photo is required and should be less then 1mb" });
+      case !allowedImageTypes.includes(photo.type):
+        return res.status(400).send({
+          error: "Invalid file format. Only JPEG, PNG, and GIF are allowed.",
+        });
+    }
+    const product = await productModel.findByIdAndUpdate(
+      req.params.pid,
+      {
+        ...req.fields,
+        slug: slugify(name),
+      },
+      { new: true }
+    );
+    if (photo) {
+      product.photo.data = fs.readFileSync(photo.path);
+      product.photo.contentType = photo.type;
+    }
+
+    res.status(201).send({
+      success: true,
+      message: "Product updated successfully",
+      product,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Error while updating product",
     });
   }
 };
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,11 +1,19 @@
 import express from "express";
 import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
-import { createProductController } from "../controllers/productController.js";
+import {
+  createProductController,
+  deleteProductController,
+  getProductController,
+  getProductPhotoController,
+  getProductsController,
+  updateProductController,
+} from "../controllers/productController.js";
 import formidable from "express-formidable";
 
 const router = express.Router();
 
 //routes
+//create product
 router.post(
   "/create-product",
   requireSignIn,
@@ -13,4 +21,31 @@ router.post(
   formidable(),
   createProductController
 );
+
+//update product
+router.put(
+  "/update-product/:pid",
+  requireSignIn,
+  isAdmin,
+  formidable(),
+  updateProductController
+);
+
+//get all products
+router.get("/get-products", getProductsController);
+
+//get single product
+router.get("/get-product/:slug", getProductController);
+
+//get photo
+router.get("/product-photo/:pid", getProductPhotoController);
+
+//delete product
+router.delete(
+  "/delete-product/:pid",
+  requireSignIn,
+  isAdmin,
+  deleteProductController
+);
+
 export default router;
